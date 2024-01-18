fab171e351cd34920a2d7aa9a745a71112860585
Thomas Berenger <berenger.thomas@gmail.com>
2024-01-18 19:21:54 +0100

    before publication

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
-import { ExampleComponent } from 'react-modal-jkf'
+import { Modal } from 'react-modal-jkf'
+import { useModal } from 'react-modal-jkf'
 import 'react-modal-jkf/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const { isShowing, toggle } = useModal()
+  return (
+    <Modal isShowing={isShowing} toggle={toggle}>
+      <p>Congratulations!</p>
+    </Modal>
+  )
 }
 
 export default App
